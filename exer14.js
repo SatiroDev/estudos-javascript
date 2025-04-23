9144c237be6992f9479d197921550cc68ed91fe5
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 19:50:43 -0300

    Update exer14.js

--- exer14.js
+++ exer14.js
@@ -1,2 +1,6 @@
-let lista = [5, 3, 8, 1, 2];
-console.log(`Lista ordenada: [${lista.sort()}]`)
+// let lista = [5, 3, 8, 1, 2];
+// console.log(`Lista ordenada: [${lista.sort()}]`) maneira errada
+
+let lista = [5, 3, 8, 1, 20];
+lista.sort((a, b) => a - b);
+console.log(`Lista ordenada: [${lista}]`); // maneira certa
