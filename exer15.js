b3eb8987b0daf6871a4a417e103332f59e6a4f39
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 19:54:00 -0300

    Update exer15.js

--- exer15.js
+++ exer15.js
@@ -1,18 +1,21 @@
-// let numero = 5;
-// var fatorial = 1;
-// for (i= 1; i <= numero; i++) {
-//     var fatorial = fatorial * i
+
+// function fat(num) {
+//     let fatorial = 1
+//     for (let i = 1; i <= num; i++) {
+//         if (true) {
+//             let fatoria = fatorial * i
+//             fatorial = fatoria
+//         }
+//     } 
+//     console.log(`fatorial de ${num}: ${fatorial}`)
 // }
-// console.log(`fatorial de ${numero}: ${fatorial}`)
+// fat(9)
 
 function fat(num) {
     let fatorial = 1
-    for (let i = 1; i <= num; i++) {
-        if (true) {
-            let fatoria = fatorial * i
-            fatorial = fatoria
-        }
-    } 
-    console.log(`fatorial de ${num}: ${fatorial}`)
+    for (let i = 1; i <= num;i ++) {
+        fatorial *= i;
+    }
+    console.log(`fatorial de ${num}: ${fatorial}`) // maneira simplificada
 }
 fat(9)
