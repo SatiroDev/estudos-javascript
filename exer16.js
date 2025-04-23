99de27161a6d25e60abeb026514d7eefabe029de
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 19:41:24 -0300

    Create exer16.js

---
+++ exer16.js
@@ -0,0 +1,15 @@
+function fibonacci(num) {
+    let atuali = 0
+    let atual = 1
+    for (let i = 1; i <= num; i ++) {
+        if (true) {
+            if (true) {
+                let atualizacao = atuali + atual
+                console.log(atualizacao)
+                atual = atuali
+                atuali = atualizacao
+            }       
+        }
+    }
+}
+fibonacci(7)
