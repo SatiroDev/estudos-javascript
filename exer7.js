2810b91b43ab871d23f146f37871d559be03bd7b
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 17:40:25 -0300

    Create exer7.js

---
+++ exer7.js
@@ -0,0 +1,10 @@
+let num = -5;
+if (num > 0) {
+    console.log(`O número ${num} é positivo`)
+}
+if (num < 0) {
+    console.log(`O número ${num} é negativo`)
+}
+if (num == 0) {
+    console.log(`O número ${num} é neutro`)
+}
