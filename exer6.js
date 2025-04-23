ab0b8ba290655d54eff5b354ff04011db16aa233
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 17:36:04 -0300

    Update exer6.js

--- exer6.js
+++ exer6.js
@@ -1,3 +1,7 @@
 for (i = 10; i >= 0; i--) {
     console.log(i)
 }
+
+// i = 10 inicia a váriavel i com o valor 10
+// i >= 0 tipo um while (enquanto i for maior ou igual a 0 ele faz o loop)
+// i-- diminui o valor de i a cada iteração
