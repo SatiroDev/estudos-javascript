0b52e94872df41d252503e72176dab560ca130c2
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 21:27:15 -0300

    Create exer24.js

---
+++ exer24.js
@@ -0,0 +1,8 @@
+let usuario = 'admin';
+let senha = '1234';
+if (usuario == 'admin' && senha == '1234') {
+    console.log('login bem-sucedido')
+}
+else {
+    console.log('usuario ou senha incorretos')
+}
