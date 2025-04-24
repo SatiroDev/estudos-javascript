7e04af688bc06378d7d62bbf8a20335fe52dfd05
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 21:30:47 -0300

    Create exer25.js

---
+++ exer25.js
@@ -0,0 +1,9 @@
+let temCupom = true;
+let eFeriado = false;
+let eClienteVip = false;
+if (temCupom == true || eFeriado == true || eClienteVip == true) {
+    console.log('cliente tem direito a desconto')
+}
+else {
+    console.log('cliente não tem direito a desconto')
+}
