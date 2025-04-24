7a5898465e6652a9e651d63a6cea520a84ab1fd0
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 21:13:07 -0300

    Update exer22.js

--- exer22.js
+++ exer22.js
@@ -1,8 +1,19 @@
+// let lista = [5, 12, 3, 21, 9];
+// let maior = 0
+// for (i in lista) {
+//     if (lista[i] >= maior) {
+//         maior = lista[i]
+//     }
+// }
+// console.log(`o maior número é ${maior}`)
+
+// outra maneira
 let lista = [5, 12, 3, 21, 9];
 let maior = 0
-for (i in lista) {
-    if (lista[i] >= maior) {
-        maior = lista[i]
+for (let num of lista) {
+    if (num >= maior) {
+        maior = num
     }
 }
+
 console.log(`o maior número é ${maior}`)
