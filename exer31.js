fdaf605b589e384df5aefde7e42bc6a1ed504481
Satiro07 <jardelsatiro57@gmail.com>
2025-04-24 14:22:42 -0300

    Create exer31.js

---
+++ exer31.js
@@ -0,0 +1,29 @@
+let diaSemana = 9;
+switch (diaSemana) {
+    case 1:
+        console.log('domingo');
+        break;
+    case 2:
+        console.log('segunda');
+        break;
+        
+    case 3:
+        console.log('terça');
+        break;
+        
+    case 4:
+        console.log('quarta');
+        break;
+        
+    case 6:
+        console.log('quinta');
+        break;
+    case 7:
+        console.log('sexta');
+        break;
+    case 8:
+        console.log('sábado');
+        break;
+    default:
+        console.log('Indisponível')
+}
