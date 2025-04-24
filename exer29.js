104da5a50338560df3b3b285948a2ac9b5e9ffc0
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 22:20:12 -0300

    Update exer29.js

--- exer29.js
+++ exer29.js
@@ -13,15 +13,21 @@ let usuario3 = {
     idade: 10,
 };
 
+let id = 0;
+
 let geral = [usuario1, usuario2, usuario3]
+for (let usuario of geral) {
+    usuario['ID'] = id
+    id += 1
+}
 
 for (let usuario of geral) {
     if (usuario['nome'] != '' && usuario['idade'] > 18) {
-        console.log(`Nome: ${usuario['nome']}`)
+        console.log(`Nome: ${usuario['nome']}, ID: ${usuario['ID']}`)
         console.log('cadastro aprovado')
     }
     else{
-        console.log(`Nome: ${usuario['nome']}`)
+        console.log(`Nome: ${usuario['nome']}, ID: ${usuario['ID']}`)
         console.log('cadastro negado')
     }
     console.log()
