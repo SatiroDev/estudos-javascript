e59380157edb2325c822c4ef94c72cfb6ff35b50
Satiro07 <jardelsatiro57@gmail.com>
2025-04-24 19:05:04 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,5 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let nomes = [];
-let i = 0;
-
-function pergunta () {
-    if (i < 4) {
-        rl.question('seu nome: ', function(resposta){
-            nomes.push(resposta);
-            i += 1;
-            pergunta();  
-        });
-    }
-    else {
-        console.log(`nomes cadastrados: ${nomes}`);
-        rl.close();
-    }
+const prompt = require('prompt-sync')();
+for (let i = 0; i <= 3; i++){
+    let nome = prompt('Qual seu nome? ');
+    console.log(`Olá, ${nome}`);
 }
-pergunta()
