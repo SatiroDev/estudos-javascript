d54383f20203810f4061009e2cf86fd8633140c0
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 21:11:33 -0300

    Update exer21.js

--- exer21.js
+++ exer21.js
@@ -1,6 +1,14 @@
+// let numeros = [4, 7, 1, 3, 9];
+// let soma = 0
+// for (i in numeros) {
+//     soma = soma + numeros[i]
+// }
+// console.log(`a soma dos números é ${soma}`)
+
+// outraa maneira
 let numeros = [4, 7, 1, 3, 9];
-let soma = 0
-for (i in numeros) {
-    soma = soma + numeros[i]
+let soma = 0;
+for (let num of numeros) {
+    soma += num
 }
 console.log(`a soma dos números é ${soma}`)
