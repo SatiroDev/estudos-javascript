6015e284119f181ff022259fe4fd8de6b5ef33df
Satiro07 <jardelsatiro57@gmail.com>
2025-04-24 16:26:39 -0300

    Create exer43.js

---
+++ exer43.js
@@ -0,0 +1,9 @@
+let alunos = [
+    {nome: 'João', nota: 8},
+    {nome: 'Maria', nota: 5},
+    {nome: 'José', nota: 9},
+    {nome: 'Clara', nota: 6}
+];
+for (let aluno of alunos) {
+    console.log(aluno);
+}
