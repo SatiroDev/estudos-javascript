70eafcb235c19b9da8e988ea3a3f6e1a907b6f57
Satiro07 <jardelsatiro57@gmail.com>
2025-04-24 17:22:47 -0300

    Update exer42.js

--- exer42.js
+++ exer42.js
@@ -1,8 +1,18 @@
+// function duplicarElementos(lista) {
+//     let dobro = [];
+//     for (let numero of lista) {
+//         dobro.push(numero*2);
+//     }
+//     return dobro;
+// }
+// console.log(duplicarElementos([1,2,3,4]))
+
+
+// mais compacto 
 function duplicarElementos(lista) {
-    let dobro = [];
-    for (let numero of lista) {
-        dobro.push(numero*2);
-    }
+    let dobro = lista.map(function(numero){
+        return numero * 2;
+    })
     return dobro;
 }
 console.log(duplicarElementos([1,2,3,4]))
