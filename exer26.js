ded1b7a74cc9866cb3a2ea13a14be8a5cf5012c7
Satiro07 <jardelsatiro57@gmail.com>
2025-04-23 21:35:22 -0300

    Create exer26.js

---
+++ exer26.js
@@ -0,0 +1,9 @@
+let preenchido = true;
+let temEmail = true;
+let temTelefone = false;
+if (preenchido == true && temEmail == true || temTelefone == true) {
+    console.log('Formulário pode ser enviado')
+}
+else {
+    console.log('Formulário nao pode ser entregue')
+}
